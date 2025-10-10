092fba75d1f22ca7c3e05f534dcf87e1d0b3f092
Vivek Sharma <viveksharma@gmail.com>
2025-10-10 10:55:46 +0530

    testing apis

--- src/app.js
+++ src/app.js
@@ -3,13 +3,24 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello now i am using nodemon and it awsome!");
+
+// app.use("/test",(req,res)=>{
+//      res.send("Hi I am from server how are you");
+// });
+
+app.get("/user",(req, res)=>{
+    res.send({"firstname":"Vivek", "LastName": "Sharma"})
+});
+app.post("/user",(req,res)=>{
+    console.log("Please send the data")
+    res.send("I saved the changes in the database")
 });
 
-app.use("/test",(req,res)=>{
-     res.send("Hi I am from server how are you");
-});
+app.delete("/user",(req, res)=>{
+    res.send("I am delete call....")
+})
+
+
 
 app.listen(3000, ()=>{
     console.log("This is my Backend server running on port 3000....")
