ffb60d26181d21956a27b7257c0eeedc4bae79b6
Vivek Sharma <viveksharma@gmail.com>
2025-11-28 12:33:30 +0530

    add options in cors

--- src/app.js
+++ src/app.js
@@ -5,7 +5,10 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
-app.use(cors());
+app.use(cors({
+    origin : "http://localhost:5173",
+    credentials: true
+}));
 app.use(cookieParser());
 app.use(express.json());
 
